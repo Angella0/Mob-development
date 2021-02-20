4f3184a25b74bd0316cce998a780eb334cf655ff
Angella0 <angellasimbwa@gmail.com>
2021-02-21 00:08:15 +0300

    first assignment

---
+++ src/hello.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var studentname = "Angella"
+    println(studentname)
+    var phonenumber = "0752853816"
+    println(phonenumber)
+    var age = 21
+    println(age)
+    var weight = "54kgs"
+    println(weight)
+    var kenyancitizen: Boolean=false
+    println(kenyancitizen)
+
+
+
+}
